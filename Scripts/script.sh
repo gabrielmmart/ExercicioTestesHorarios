b7f08f587b479a5ca04c848a88f0dd63ffb836f9
gabrielmmart <gabriel.medeiros@ges.inatel.br>
2023-04-09 13:23:29 -0300

    Update script.sh

--- Scripts/script.sh
+++ Scripts/script.sh
@@ -1,4 +1,5 @@
 cd ExercicioDosHorarios
 sudo apt install mailutils
 echo "Pipeline executado"
+echo ${NOTIF_EMAIL}
 echo "Mandando e-mail com mail do linux" | mail -s "meu email deu tudo certo" ${NOTIF_EMAIL}
